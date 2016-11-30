b4ca78e9681ad60d090c502e2c26fbb5e3abdcbe
EmiM <emi.marczyk@gmail.com>
2016-11-30 16:50:09 +0100

    Proper generating features' file for all number images

--- rozp1/rozp1/generate_moments.cpp
+++ rozp1/rozp1/generate_moments.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <cstdio>
 #include <set>
+#include <iomanip>
 
 using namespace cv;
 using namespace std;
@@ -11,6 +12,7 @@ using namespace std;
 int const max_BINARY_value = 255;
 int threshold_value = 0;
 int threshold_type = 0;
+int PRECISION = 6;
 
 string get_filename(int number, int person, int number_repetition) {
 	string filename = "data/digit";
@@ -57,6 +59,10 @@ vector<pair<Mat, int>> get_images() {
 	return images_numbers;
 }
 
+void save_feature(ofstream &file, int feature_number, double feature) {
+	file << feature_number << ":" << fixed << setprecision(PRECISION) << feature << " ";
+}
+
 int main(int argc, char** argv)
 {
 	ofstream features;
@@ -73,17 +79,15 @@ int main(int argc, char** argv)
 		double hum[7];
 		HuMoments(mm, hum);
 
-		for (int h = 0; h < 7; h++) {
-			cout << hum[h] << endl;
-		}
-
 		// ograniczyæ wyniki po przecinku
 		// zapisaæ do pliku (31 dla ka¿dej linii)
 		features.open("features_output.txt", fstream::app);
-		features << image.second << " ";
+		features << image.second + 1 << " ";
 		int h;
 		for (h = 0; h < 7; h++) {
-			features << h + 1 << ":" << hum[h] << " ";
+			//save_feature(features, h+1, hum[h]);
+			int line = h + 1;
+			features << line << ":" << hum[h] << " ";
 		}
 
 		features << ++h << ":" << mm.m00 << " ";
@@ -116,67 +120,6 @@ int main(int argc, char** argv)
 		features.close();
 
 	}
-
-	/*Mat src = imread("digits00_0.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-	if (!src.data) {
-		std::cout << "OPS" << std::endl;
-		return 0;
-	}*/
-
-	// binary
-	//threshold(src, binary_img, threshold_value, max_BINARY_value, THRESH_BINARY + THRESH_OTSU);
-
-	// invert
-	//threshold(binary_img, inverted_img, threshold_value, max_BINARY_value, THRESH_BINARY_INV);
-
-	// count moments
-	/*Moments mm = moments(inverted_img);
-
-	// humoment
-	double hum[7];
-	HuMoments(mm, hum);
-
-	for (int h = 0; h < 7; h++) {
-		cout << hum[h] << endl;
-	}
-
-	// ograniczyæ wyniki po przecinku
-	// zapisaæ do pliku (31 dla ka¿dej linii)
-	features.open("features_output.txt");
-	features << "1 ";
-	int h;
-	for (h = 0; h < 7; h++) {
-		features << h + 1 << ":" << hum[h] << " ";
-	}
-	
-	features << ++h << ":" << mm.m00 << " ";
-	features << ++h << ":" << mm.m01 << " ";
-	features << ++h << ":" << mm.m02 << " ";
-	features << ++h << ":" << mm.m03 << " ";
-	features << ++h << ":" << mm.m10 << " ";
-	features << ++h << ":" << mm.m11 << " ";
-	features << ++h << ":" << mm.m12 << " ";
-	features << ++h << ":" << mm.m20 << " ";
-	features << ++h << ":" << mm.m21 << " ";
-	features << ++h << ":" << mm.m30 << " ";
-	features << ++h << ":" << mm.mu02 << " ";
-	features << ++h << ":" << mm.mu03 << " ";
-	features << ++h << ":" << mm.mu11 << " ";
-	features << ++h << ":" << mm.mu12 << " ";
-	features << ++h << ":" << mm.mu20 << " ";
-	features << ++h << ":" << mm.mu21 << " ";
-	features << ++h << ":" << mm.mu30 << " ";
-
-	features << ++h << ":" << mm.nu02 << " ";
-	features << ++h << ":" << mm.nu03 << " ";
-	features << ++h << ":" << mm.nu11 << " ";
-	features << ++h << ":" << mm.nu12 << " ";
-	features << ++h << ":" << mm.nu20 << " ";
-	features << ++h << ":" << mm.nu21 << " ";
-	features << ++h << ":" << mm.nu30 << " ";
-
-	features << "\n";
-	features.close();*/
 
 	/*namedWindow("binary", CV_WINDOW_AUTOSIZE);
 	namedWindow("original", CV_WINDOW_AUTOSIZE);
